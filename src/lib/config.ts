9714dc94c364c52b85d86618edeaaaf1dee67321
Arthur Albarelli <arthuralbarelli@gmail.com>
2023-07-11 22:01:58 -0300

    fix(src/lib/config.ts): Deduplicate title const - Rename second 'title' const to 'description'.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment'
 
 export const title = 'Arthur Albarelli'
-export const title = 'A blog about technology'
+export const description = 'A blog about technology'
 export const url = dev ? 'http://localhost:5173/' : 'https://arthuralbarelli.dev'
