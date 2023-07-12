128366c8c1cd620bfe805f2dc0ad36ba7cfc27de
Arthur Albarelli <arthuralbarelli@gmail.com>
2023-07-12 06:15:23 -0300

    feat(src/routes): Add header file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import Header from './header.svelte'
+</script>
+
+<Header />
+
 <nav>
 		<a href="/">Home</a>
 		<a href="/posts">Posts</a>
---
+++ src/routes/header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import * as config from '$lib/config'
+</script>
+
+<nav>
+	<!--Title-->
+	<a href="/" class="title">
+		<b>{config.title}</b>
+	</a>
+</nav>
