d92edd6e73198ac0eb1e9dc7405cba3a3f19c065
Arthur Albarelli <arthuralbarelli@gmail.com>
2023-07-16 21:57:51 -0300

    feat(src): Add file to show posts in front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,23 @@
-<style>
-	main {
-		padding: 4vw;
-	}
-</style>
+<script lang="ts">
+	import { formatDate } from '$lib/utils'
+	import * as config from '$lib/config'
 
-<main>
-	<h1>Posts</h1>
-	<p>Here we will place all the different blog posts</p>
-</main>
+	export let data
+</script>
+
+<svelte:head>
+	<title>{config.title}</title>
+</svelte:head>
+
+<!-- Posts -->
+<section>
+	<ul class="posts">
+		{#each data.posts as post}
+			<li class="post">
+				<a href={post.slug} class="title">{post.title}</a>
+				<p class="date">{formatDate(post.date)}</p>
+				<p class="description">{post.description}</p>
+			</li>
+		{/each}
+	</ul>
+</section>
