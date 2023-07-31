0866b7bfddf19de4af9b5fe93771a896b2102347
Arthur Albarelli <77936055+arthuralbarelli@users.noreply.github.com>
2023-07-31 14:04:46 -0300

    feat(header.svelte): Add Home and Posts link to header file (#2)
    - Also remove this links from '+layout.svelte' file

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -7,4 +7,24 @@
 	<a href="/" class="title">
 		<b>{config.title}</b>
 	</a>
+
+	<ul class="links">
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/posts">Posts</a>
+		</li>
+	</ul>
 </nav>
+
+<style>
+	ul {
+		display: flex;
+		list-style-type: none;
+	}
+
+	li {
+		padding: 5px
+	}
+</style>
